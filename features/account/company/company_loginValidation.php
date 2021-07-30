e0bf838df38196c26d0ee48905ddfce960db1c3a
nujhatnower001 <you@example.com>
2021-07-30 23:24:25 +0600

    Signup/ SignIn for company and jobseeker

---
+++ features/account/company/company_loginValidation.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+$error = "";
+// store session data
+if (isset($_POST['signin'])) {
+	
+    if (empty($_POST['email']) || empty($_POST['password'])) {
+        $error = '<div class="alert alert-danger">Email or password is invalid!.</div>';
+        header("location: company_login.php?error=$error");
+    } else {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $conn = new mysqli("localhost", "root", "", "the_right_job");
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+		$sql = "SELECT * FROM company WHERE email='" . $email . "' AND password='" . $password . "'";
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+			
+            $_SESSION["email"] = $email;
+            header("location: ../loginsuccess.php?error=$error");
+			
+			
+			
+        } else {
+            
+            $error = '<div class="alert alert-danger">Email or password is invalid!.</div>';
+            header("location: company_login.php?error=$error");
+        }
+        $conn->close();
+    }
+}
---
+++ features/account/company/createacc.php
@@ -0,0 +1,75 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	
+	$conn = new mysqli("localhost", "root", "", "the_right_job");
+
+        // If file upload form is submitted
+        $company_name = $_POST["company_name"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $confirm_password = $_POST["confirm_password"];
+        $profile_description = $_POST['profile_description'];
+        $address = $_POST['address'];
+       
+       
+       
+       
+     $duplicate=mysqli_query($conn,"select * from company where company_name='$company_name' or email='$email'");
+    
+    
+ if (mysqli_num_rows($duplicate)>0)
+{
+    $error = '<div class="alert alert-danger">Duplicate Exists!.</div>';
+    
+    header("Location: signup.php?error=$error");
+}
+elseif($password != $confirm_password){
+    $error = '<div class="alert alert-danger">Password and confirm password does not match. Try Again</div>';
+    
+    header("Location: signup.php?error=$error");
+}
+
+
+else{
+
+        $servername = "localhost";
+        $username = "root";
+        $pass = "";
+try {
+            
+
+            $conn = new PDO("mysql:host=$servername;dbname=the_right_job", $username, $pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                
+            $sql = "INSERT into company (`company_name`,`email`, `password`, `profile_description`, `address`) VALUES ('$company_name','$email','$password','$profile_description','$address')";
+
+            $conn->exec($sql);
+            $error = '<div class="alert alert-success">Thank you for your register. You can login now.</div>';
+            
+            header("Location: signup.php?error=$error");
+}
+                 
+
+    catch(PDOException $e)
+        {
+      echo $sql . "
+      " . $e->getMessage();
+    }
+    $conn = null;
+
+}
+
+}
+
+            
+        
+
+
+
+
+	
+
+
+    ?>
